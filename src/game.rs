b5e358216e261f280dd2407ecfa3df7331a2b0db
Thomas Zachary <thomaszachary06@proton.me>
2025-04-04 15:57:01 +0100

    added apply_move function to state

--- src/game.rs
+++ src/game.rs
@@ -44,3 +44,98 @@ enum State {
     Stage4DealerPicked(card::Card, card::Card, card::Card, card::Suit, card::Card),
     Finished(u32),
 }
+impl State {
+    fn apply_move(&self, mov: Move) -> Option<Self> {
+        match self {
+            State::Start => {
+                if let Move::Colour(colour) = mov {
+                    Some(State::Stage1PlayerPicked(colour))
+                } else {
+                    None
+                }
+            }
+            State::Stage1PlayerPicked(colour) => {
+                if let Move::Card(card) = mov {
+                    Some(State::Stage1DealerPicked(*colour, card))
+                } else {
+                    None
+                }
+            }
+            State::Stage1DealerPicked(colour, card) => {
+                if colour != &card.suit.colour() {
+                    Some(State::Finished(0))
+                } else if let Move::HiLo(hi_lo) = mov {
+                    Some(State::Stage2PlayerPicked(*card, hi_lo))
+                } else if let Move::Finish = mov {
+                    Some(State::Finished(2))
+                } else {
+                    None
+                }
+            }
+            State::Stage2PlayerPicked(card, hi_lo) => {
+                if let Move::Card(card1) = mov {
+                    Some(State::Stage2DealerPicked(*card, *hi_lo, card1))
+                } else {
+                    None
+                }
+            }
+            State::Stage2DealerPicked(card, hi_lo, card1) => {
+                if !hi_lo.is_true(card1, card) {
+                    Some(State::Finished(0))
+                } else if let Move::InOut(in_out) = mov {
+                    Some(State::Stage3PlayerPicked(*card, *card1, in_out))
+                } else if let Move::Finish = mov {
+                    Some(State::Finished(6))
+                } else {
+                    None
+                }
+            }
+            State::Stage3PlayerPicked(card, card1, in_out) => {
+                if let Move::Card(card2) = mov {
+                    Some(State::Stage3DealerPicked(*card, *card1, *in_out, card2))
+                } else {
+                    None
+                }
+            }
+            State::Stage3DealerPicked(card, card1, in_out, card2) => {
+                if !in_out.is_true(card, card2, card1) {
+                    Some(State::Finished(0))
+                } else if let Move::Suit(suit) = mov {
+                    Some(State::Stage4PlayerPicked(*card, *card1, *card2, suit))
+                } else if let Move::Finish = mov {
+                    Some(State::Finished(24))
+                } else {
+                    None
+                }
+            }
+            State::Stage4PlayerPicked(card, card1, card2, suit) => {
+                if let Move::Card(card3) = mov {
+                    Some(State::Stage4DealerPicked(
+                        *card, *card1, *card2, *suit, card3,
+                    ))
+                } else {
+                    None
+                }
+            }
+            State::Stage4DealerPicked(card, card1, card2, suit, card3) => {
+                if suit != &card3.suit {
+                    Some(State::Finished(0))
+                } else if let Move::Finish = mov {
+                    Some(State::Finished(480))
+                } else {
+                    None
+                }
+            }
+            State::Finished(_) => None,
+        }
+    }
+}
+
+enum Move {
+    Colour(card::Colour),
+    HiLo(HiLo),
+    InOut(InOut),
+    Suit(card::Suit),
+    Card(card::Card),
+    Finish,
+}
