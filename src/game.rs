04f28b5d095ecccefd6665f915add20e6119b925
Thomas Zachary <thomaszachary06@proton.me>
2025-04-04 13:40:38 +0100

    added helper functions to HiLo and InOut

--- src/game.rs
+++ src/game.rs
@@ -5,11 +5,30 @@ enum HiLo {
     Higher,
     Lower,
 }
+impl HiLo {
+    fn is_true(&self, card0: &card::Card, card1: &card::Card) -> bool {
+        match self {
+            Self::Higher => card0.value > card1.value,
+            Self::Lower => card0.value < card1.value,
+        }
+    }
+}
 
 #[derive(Clone, Copy)]
 enum InOut {
     Inside,
     Outside,
+}
+impl InOut {
+    fn is_true(&self, card0: &card::Card, card1: &card::Card, card2: &card::Card) -> bool {
+        let small = Ord::min(card0.value, card2.value);
+        let large = Ord::max(card0.value, card2.value);
+
+        match self {
+            Self::Inside => small < card1.value && card1.value < large,
+            Self::Outside => card1.value < small || large < card1.value,
+        }
+    }
 }
 
 #[derive(Clone, Copy)]
